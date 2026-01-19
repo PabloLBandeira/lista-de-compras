654bb2b16a68d7a0d16cb5ae764cd8e15204d2e1
BandeiraPablo <pablo.bandeira@arcasoltec.com.br>
2026-01-19 16:58:36 -0300

    chore(i18n): define pt-BR como locale padrão da aplicação

--- lista-de-compras/config/application.rb
+++ lista-de-compras/config/application.rb
@@ -23,5 +23,10 @@ module ListaDeCompras
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Internacionalização
+    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = [:'pt-BR', :en]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
